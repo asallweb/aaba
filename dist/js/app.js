bade2574cbcde00a544fb02d7932bf7455cf4c9f
Oleksii <asallweb@gmail.com>
2023-01-18 09:57:38 +0100

    v0.40

--- dist/js/app.js
+++ dist/js/app.js
@@ -3756,158 +3756,6 @@
                 loadInSlide
             });
         }
-        function Autoplay(_ref) {
-            let {swiper, extendParams, on, emit} = _ref;
-            let timeout;
-            swiper.autoplay = {
-                running: false,
-                paused: false
-            };
-            extendParams({
-                autoplay: {
-                    enabled: false,
-                    delay: 3e3,
-                    waitForTransition: true,
-                    disableOnInteraction: true,
-                    stopOnLastSlide: false,
-                    reverseDirection: false,
-                    pauseOnMouseEnter: false
-                }
-            });
-            function run() {
-                const $activeSlideEl = swiper.slides.eq(swiper.activeIndex);
-                let delay = swiper.params.autoplay.delay;
-                if ($activeSlideEl.attr("data-swiper-autoplay")) delay = $activeSlideEl.attr("data-swiper-autoplay") || swiper.params.autoplay.delay;
-                clearTimeout(timeout);
-                timeout = utils_nextTick((() => {
-                    let autoplayResult;
-                    if (swiper.params.autoplay.reverseDirection) if (swiper.params.loop) {
-                        swiper.loopFix();
-                        autoplayResult = swiper.slidePrev(swiper.params.speed, true, true);
-                        emit("autoplay");
-                    } else if (!swiper.isBeginning) {
-                        autoplayResult = swiper.slidePrev(swiper.params.speed, true, true);
-                        emit("autoplay");
-                    } else if (!swiper.params.autoplay.stopOnLastSlide) {
-                        autoplayResult = swiper.slideTo(swiper.slides.length - 1, swiper.params.speed, true, true);
-                        emit("autoplay");
-                    } else stop(); else if (swiper.params.loop) {
-                        swiper.loopFix();
-                        autoplayResult = swiper.slideNext(swiper.params.speed, true, true);
-                        emit("autoplay");
-                    } else if (!swiper.isEnd) {
-                        autoplayResult = swiper.slideNext(swiper.params.speed, true, true);
-                        emit("autoplay");
-                    } else if (!swiper.params.autoplay.stopOnLastSlide) {
-                        autoplayResult = swiper.slideTo(0, swiper.params.speed, true, true);
-                        emit("autoplay");
-                    } else stop();
-                    if (swiper.params.cssMode && swiper.autoplay.running) run(); else if (false === autoplayResult) run();
-                }), delay);
-            }
-            function start() {
-                if ("undefined" !== typeof timeout) return false;
-                if (swiper.autoplay.running) return false;
-                swiper.autoplay.running = true;
-                emit("autoplayStart");
-                run();
-                return true;
-            }
-            function stop() {
-                if (!swiper.autoplay.running) return false;
-                if ("undefined" === typeof timeout) return false;
-                if (timeout) {
-                    clearTimeout(timeout);
-                    timeout = void 0;
-                }
-                swiper.autoplay.running = false;
-                emit("autoplayStop");
-                return true;
-            }
-            function pause(speed) {
-                if (!swiper.autoplay.running) return;
-                if (swiper.autoplay.paused) return;
-                if (timeout) clearTimeout(timeout);
-                swiper.autoplay.paused = true;
-                if (0 === speed || !swiper.params.autoplay.waitForTransition) {
-                    swiper.autoplay.paused = false;
-                    run();
-                } else [ "transitionend", "webkitTransitionEnd" ].forEach((event => {
-                    swiper.$wrapperEl[0].addEventListener(event, onTransitionEnd);
-                }));
-            }
-            function onVisibilityChange() {
-                const document = ssr_window_esm_getDocument();
-                if ("hidden" === document.visibilityState && swiper.autoplay.running) pause();
-                if ("visible" === document.visibilityState && swiper.autoplay.paused) {
-                    run();
-                    swiper.autoplay.paused = false;
-                }
-            }
-            function onTransitionEnd(e) {
-                if (!swiper || swiper.destroyed || !swiper.$wrapperEl) return;
-                if (e.target !== swiper.$wrapperEl[0]) return;
-                [ "transitionend", "webkitTransitionEnd" ].forEach((event => {
-                    swiper.$wrapperEl[0].removeEventListener(event, onTransitionEnd);
-                }));
-                swiper.autoplay.paused = false;
-                if (!swiper.autoplay.running) stop(); else run();
-            }
-            function onMouseEnter() {
-                if (swiper.params.autoplay.disableOnInteraction) stop(); else {
-                    emit("autoplayPause");
-                    pause();
-                }
-                [ "transitionend", "webkitTransitionEnd" ].forEach((event => {
-                    swiper.$wrapperEl[0].removeEventListener(event, onTransitionEnd);
-                }));
-            }
-            function onMouseLeave() {
-                if (swiper.params.autoplay.disableOnInteraction) return;
-                swiper.autoplay.paused = false;
-                emit("autoplayResume");
-                run();
-            }
-            function attachMouseEvents() {
-                if (swiper.params.autoplay.pauseOnMouseEnter) {
-                    swiper.$el.on("mouseenter", onMouseEnter);
-                    swiper.$el.on("mouseleave", onMouseLeave);
-                }
-            }
-            function detachMouseEvents() {
-                swiper.$el.off("mouseenter", onMouseEnter);
-                swiper.$el.off("mouseleave", onMouseLeave);
-            }
-            on("init", (() => {
-                if (swiper.params.autoplay.enabled) {
-                    start();
-                    const document = ssr_window_esm_getDocument();
-                    document.addEventListener("visibilitychange", onVisibilityChange);
-                    attachMouseEvents();
-                }
-            }));
-            on("beforeTransitionStart", ((_s, speed, internal) => {
-                if (swiper.autoplay.running) if (internal || !swiper.params.autoplay.disableOnInteraction) swiper.autoplay.pause(speed); else stop();
-            }));
-            on("sliderFirstMove", (() => {
-                if (swiper.autoplay.running) if (swiper.params.autoplay.disableOnInteraction) stop(); else pause();
-            }));
-            on("touchEnd", (() => {
-                if (swiper.params.cssMode && swiper.autoplay.paused && !swiper.params.autoplay.disableOnInteraction) run();
-            }));
-            on("destroy", (() => {
-                detachMouseEvents();
-                if (swiper.autoplay.running) stop();
-                const document = ssr_window_esm_getDocument();
-                document.removeEventListener("visibilitychange", onVisibilityChange);
-            }));
-            Object.assign(swiper.autoplay, {
-                pause,
-                run,
-                start,
-                stop
-            });
-        }
         function effect_init_effectInit(params) {
             const {effect, swiper, on, setTranslate, setTransition, overwriteParams, perspective, recreateShadows, getEffectParams} = params;
             on("beforeInit", (() => {
@@ -4030,8 +3878,9 @@
         function initSliders() {
             if (document.querySelector(".swiper")) {
                 const progressBar = document.querySelector(".portfolio__progress-fill");
+                const slideImg = document.querySelector(".portfolio__slide-img");
                 const swiper = new core(".swiper", {
-                    modules: [ Navigation, Autoplay, Lazy, EffectFade ],
+                    modules: [ Navigation, Lazy, EffectFade ],
                     observer: true,
                     observeParents: true,
                     slidesPerView: 1,
@@ -4047,7 +3896,7 @@
                         loadPrevNext: true
                     },
                     effect: "fade",
-                    loop: false
+                    loop: true
                 });
                 progressBar.addEventListener("animationend", myEndFunction);
                 function myEndFunction() {
@@ -4055,11 +3904,15 @@
                     progressBar.style.animation = "none";
                     void progressBar.offsetWidth;
                     progressBar.style.animation = null;
+                    slideImg.style.animation = "none";
+                    slideImg.style.animation = null;
                 }
                 swiper.on("slideChange", (function() {
                     progressBar.style.animation = "none";
                     void progressBar.offsetWidth;
                     progressBar.style.animation = null;
+                    slideImg.style.animation = "none";
+                    slideImg.style.animation = null;
                 }));
             }
         }
